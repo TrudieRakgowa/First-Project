71682d7d8d9e334c21d6781539d5d60da00b0ad2
RC_Student_lab <RC_Student_lab@10.0.0.25>
2024-09-10 11:26:30 +0000

    First commit

---
+++ src/main/java/com/mycompany/trudie/TRUDIE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trudie;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class TRUDIE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
